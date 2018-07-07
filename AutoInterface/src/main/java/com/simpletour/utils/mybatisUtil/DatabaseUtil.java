6b8a81b6a29db19112243915d289f2701861a222
lizhipeng <236796974@qq.com>
2018-07-07 23:36:55 +0800

    创建mybatis配置，version1

---
+++ AutoInterface/src/main/java/com/simpletour/utils/mybatisUtil/DatabaseUtil.java
@@ -0,0 +1,12 @@
+package com.simpletour.utils.mybatisUtil;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+public class DatabaseUtil {
+    Reader reader = Resource.getResourceAsReader
+}
